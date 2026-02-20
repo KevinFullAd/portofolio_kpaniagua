11530a3641d7324eb6bdbd482c13002ca87e2351
Kevin Paniagua <paniaguakevin2805@gmail.com>
2026-02-20 20:46:18 -0300

    fix: dios salvame

--- api/contact.js
+++ api/contact.js
@@ -2,6 +2,7 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+<<<<<<< HEAD
 // Rate limit simple en memoria
 let lastRequestTime = 0;
 
@@ -94,4 +95,38 @@ ${message}
       { status: 500 }
     );
   }
+=======
+export default async function handler(req, res) {
+    if (req.method !== "POST") {
+        return res.status(405).json({ error: "Method not allowed" });
+    }
+
+    const { firstName, lastName, email, subject, message, area } = req.body;
+
+    if (!firstName || !email || !message) {
+        return res.status(400).json({ error: "Missing fields" });
+    }
+
+    try {
+        await resend.emails.send({
+            from: "onboarding@resend.dev",
+            to: "paniagua.kevin.damian@gmail.com",
+            subject: subject || "Nuevo mensaje desde portfolio",
+            reply_to: email,
+            text: `
+                Nombre: ${firstName} ${lastName}
+                Email: ${email}
+                Área: ${area}
+
+                Mensaje:
+                ${message}
+                    `,
+        });
+
+        return res.status(200).json({ success: true });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: "Error sending email" });
+    }
+>>>>>>> 08523bf (feat:ultimo cambio)
 }
