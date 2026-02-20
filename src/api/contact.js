cca3c0d14823513ed69fcfc44bae7c2071474070
Kevin Paniagua <paniaguakevin2805@gmail.com>
2026-02-20 20:44:05 -0300

    feat: Se agrega Resend para el envio de datos del formulario

--- src/api/contact.js
+++ src/api/contact.js
@@ -1,38 +1,37 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async function handler(req, res) {
     if (req.method !== "POST") {
-        return res.status(405).json({ message: "Method not allowed" });
+        return res.status(405).json({ error: "Method not allowed" });
     }
 
     const { firstName, lastName, email, subject, message, area } = req.body;
 
+    if (!firstName || !email || !message) {
+        return res.status(400).json({ error: "Missing fields" });
+    }
+
     try {
-        const transporter = nodemailer.createTransport({
-            service: "gmail",
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS,
-            },
-        });
-
-        await transporter.sendMail({
-            from: `"Portfolio Contact" <${process.env.EMAIL_USER}>`,
-            to: process.env.EMAIL_USER,
+        await resend.emails.send({
+            from: "onboarding@resend.dev",
+            to: "paniagua.kevin.damian@gmail.com",
             subject: subject || "Nuevo mensaje desde portfolio",
+            reply_to: email,
             text: `
-Nombre: ${firstName} ${lastName}
-Email: ${email}
-Área: ${area}
+                Nombre: ${firstName} ${lastName}
+                Email: ${email}
+                Área: ${area}
 
-Mensaje:s
-${message}
-            `,
+                Mensaje:
+                ${message}
+                    `,
         });
 
         return res.status(200).json({ success: true });
     } catch (error) {
         console.error(error);
-        return res.status(500).json({ success: false });
+        return res.status(500).json({ error: "Error sending email" });
     }
 }
